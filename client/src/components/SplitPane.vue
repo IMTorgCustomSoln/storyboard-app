ec938d4f58f338f8766b6d75c79fec0b373cf3b0
jason.beach <jason.beach@jasonbeachs-MBP.fios-router.home>
2024-01-10 01:09:33 -0500

    mod to improve outline style

--- client/src/components/SplitPane.vue
+++ client/src/components/SplitPane.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="panel">
+  <div class="panel">
     <Splitpanes >
       <Pane min-size="5">
         <slot name="outline"></slot>
@@ -11,7 +11,7 @@
         <slot name="layout"></slot>
       </Pane>
     </Splitpanes>
-</div>
+  </div>
 </template>
 
 <script>
@@ -45,6 +45,15 @@ export default{
 
 <style scoped>
 
+.panel {
+    height: 100%;
+    margin: 0;
+}
+
+.splitpanes {
+  background-color: #f8f8f8;
+}
+
 .splitpanes__pane {
   box-shadow: 0 0 5px rgba(0, 0, 0, .2) inset;
   display: block; 
@@ -54,6 +63,42 @@ export default{
   /*display: flex;*/
   position: relative;
 }
+
+
+
+
+/* increase for touchscreen */
+
+.splitpanes__splitter {
+  background-color: #ccc;
+  position: relative;
+}
+.splitpanes__splitter:before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: opacity 0.4s;
+  background-color: rgba(255, 0, 0, 0.3);
+  opacity: 0;
+  z-index: 1;
+}
+.splitpanes__splitter:hover:before {
+  /*opacity: 1;*/
+  background-color: rgba(255, 0, 0, 0.3);
+}
+.splitpanes--vertical > .splitpanes__splitter:before {
+  left: -30px;right: -30px;height: 100%;
+}
+.splitpanes--horizontal > .splitpanes__splitter:before {
+  top: -30px;bottom: -30px;width: 100%;
+}
+
+
+
+
+
+
 
 em.specs {
   font-size: 0.2em;
@@ -66,88 +111,4 @@ em.specs {
   text-align: center;
 }
 
-
-/* GENERAL STYLES 
-html, body, #app {height: 100%;margin: 0;}
-body {
-  font-family: Helvetica, Arial, sans-serif;
-  color: rgba(255, 255, 255, 0.6);
-  font-size: 5em;
-}
-
-/* documentation link 
-p {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
-  color: #666;
-  z-index: 10;
-  font-size: 12px;
-  
-  a {color: inherit;}
-}
-*/
-
-
-/* increase for touchscreen 
-
-.splitpanes {
-  background-color: #f2f2f2;
-
-&__pane {
-  justify-content: center;
-  align-items: top;
-  display: flex;
-}
-
-&__splitter {
-  background-color: #ccc;
-  position: relative;
-}
-
-&__splitter:before {
-  content: '';
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  transition: 0.4s;
-}
-&--vertical > &__splitter:before {left: -30px;right: -30px;}
-&--horizontal > &__splitter:before {top: -30px;bottom: -30px;}
-&__splitter:hover:before {background-color: rgba(255, 0, 0, 0.3);}
-}
-*/
-
-.panel {
-    height: 100%;
-    margin: 0;
-}
-
-
-/*
-.panel {
-    height: 100%;
-    margin: 0;
-font-family: Helvetica, Arial, sans-serif;
-color: rgba(255, 255, 255, 0.8);
-font-size: 5em;
-}
-
-
-.panel .text {
-color: #999;
-font-size: 15px;
-text-align: center;
-}
-
-.panel .text em {
-display: block;
-margin-top: 0.5em;
-color: #bbb;
-font-size: 13px;
-text-align: center;
-}
-*/
 </style>
